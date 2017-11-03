0e25c5c89431ebdb7875fdde1b82c7e90c5619ad
Atul <atul.bhombe@gmail.com>
2017-11-03 09:16:06 +0530

    Add websocket sample

---
+++ src/main/java/com/kafka/web/kafkawebsocket/KafkaWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.kafka.web.kafkawebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWebsocketApplication.class, args);
+	}
+}
